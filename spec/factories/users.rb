8021b64413c8232171a89f2f7bf1c106bb684d5c
sylveonpoke <playingdigimonlinkz881@ezweb.ne.jp>
2022-01-09 18:39:49 +0900

    テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    nickname              { Faker::Name.initials(number: 2) }
+    family_name           { person.last.kanji }
+    first_name            { person.first.kanji }
+    family_name_kana      { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    date_of_birth         { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
+    password_confirmation { password }
+  end
+end
