d6bd7039a6765015c8eb78cb1f6fdc058026a2d4
sylveonpoke <playingdigimonlinkz881@ezweb.ne.jp>
2022-01-10 08:21:57 +0900

    jsによる販売手数料&利益計算

---
+++ app/javascript/price.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(){
+  const priceInput = document.getElementById("item-price")
+    priceInput.addEventListener('input', function(){
+      const addTaxDom = document.getElementById("add-tax-price")
+      addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 )
+      const addProfitDom = document.getElementById("profit")
+      addProfitDom.innerHTML = Math.floor(priceInput.value - Math.round(priceInput.value * 0.1 ))
+  })
+})
